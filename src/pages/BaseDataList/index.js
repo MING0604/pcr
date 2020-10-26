4bfcbfcc082366d5a2e02c0f4663c1bdf9e77971
MING0604 <1206710179@qq.com>
2020-10-26 23:00:34 +0800

    添加查看基础数据（boss列表以及角色列表）功能

--- src/pages/BaseDataList/index.js
+++ src/pages/BaseDataList/index.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { List, Button, Divider } from 'antd';
+import { Link } from 'react-router-dom'
+
+import './index.css'
 
 class BaseDataList extends Component {
     constructor(props) {
@@ -8,16 +13,68 @@ class BaseDataList extends Component {
             
         }
     }
-
-    render() {
+    componentWillMount(){
         let ifLogin = localStorage.getItem('ifLogin')
         if(!ifLogin){
             this.props.history.push('/login')
         }
+    }
+    render() {
+        const bossList = this.props.bossData.map((bossItem)=>{
+            let stage = String.fromCharCode(65+bossItem.stage-1)
+            return `${stage}${bossItem.bossId}${bossItem.name}`
+        })
+        
+        const characterList = this.props.characterData.map((characterItem)=>{
+            return characterItem.name
+        })
         return (
-            <div>BaseData</div>
+            <div className="base-data-list">
+                <Button type="primary" className='switch-btn'>
+                    <Link to='/'>返回首页</Link>
+                </Button>
+                <Divider orientation="left">Boss列表</Divider>
+                <List
+                    className="list"
+                    size="small"
+                    bordered
+                    dataSource={bossList}
+                    footer={<div>Footer</div>}
+                    renderItem={item => (
+                        <List.Item>
+                            {item}
+                        </List.Item>
+                    )}
+                    />
+                <Divider orientation="left">角色列表</Divider>
+                <List
+                    className="list"
+                    size="small"
+                    bordered
+                    dataSource={characterList}
+                    footer={<div>Footer</div>}
+                    renderItem={item => (
+                        <List.Item>
+                            {item}
+                        </List.Item>
+                    )}
+                    />
+            </div>
         )
     }
 }
 
-export default BaseDataList
+const mapSateToProps = (state)=>{
+    return {
+        bossData:state.bossData,
+        characterData:state.characterData
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        
+    }
+}
+
+export default connect(mapSateToProps,mapDispatchToProps)(BaseDataList)
